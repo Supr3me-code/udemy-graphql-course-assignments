6cf5344ef2ec6ac12ed9065c7278e197d3ba939d
Dishant Arora <dishantarora16@gmail.com>
2023-03-12 23:17:02 +0530

    added delete song mutation

--- lyrical/client/components/SongList.js
+++ lyrical/client/components/SongList.js
@@ -1,30 +1,55 @@
 import React from "react";
 import gql from "graphql-tag";
-import { graphql } from "react-apollo";
+import { compose, graphql } from "react-apollo";
 import { Link } from "react-router";
 import fetchSongsQuery from "../queries/fetchSongs";
+import deleteSongQuery from "../queries/deleteSong";
 
 const SongList = (props) => {
   const songs = props.data.songs;
   let loading = props.data.loading;
+  const deleteSongHandler = (id) => {
+    // console.log(id, "DELETED");
+    props
+      .mutate({
+        variables: {
+          id,
+        },
+      })
+      .then(() => props.data.refetch());
+  };
+
   return (
     <div>
       <ul className="collection">
         {!loading ? (
-          songs.map((song, key) => (
-            <li key={song.id} className="collection-item">
-              {song.title}
+          songs.map(({id, title}) => (
+            <li key={id} className="collection-item">
+              {title}
+              <i
+                style={{ cursor: "pointer" }}
+                className="material-icons"
+                onClick={() => deleteSongHandler(id)}
+              >
+                {console.log(id, title)}
+                delete
+              </i>
             </li>
           ))
         ) : (
           <div>loading...</div>
         )}
       </ul>
-      <Link to="/songs/new" className="btn-floating btn-large red right">
+      <Link to="/songs/new" className="btn-floating btn-large green right">
         <i className="material-icons">add</i>
       </Link>
     </div>
   );
 };
 
-export default graphql(fetchSongsQuery)(SongList);
+export default graphql(deleteSongQuery)(graphql(fetchSongsQuery)(SongList));
+
+// export default compose(
+//   graphql(fetchSongsQuery),
+//   graphql(deleteSongQuery)
+// )(SongList);
