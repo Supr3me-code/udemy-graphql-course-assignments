c1e830b865e4289979076cc449b2e40bca9c5b19
Dishant Arora <dishantarora16@gmail.com>
2023-03-13 15:13:49 +0530

    added likeLyric mutation

--- lyrical/client/components/LyricList.js
+++ lyrical/client/components/LyricList.js
@@ -1,15 +1,28 @@
 import React from "react";
+import { graphql } from "react-apollo";
+import likeLyric from "../queries/likeLyric";
 
 const LyricList = (props) => {
+  const likeHandler = (id) => {
+    props.mutate({
+      variables: { id },
+    });
+  };
   return (
     <ul className="collection">
-      {props.lyrics.map(({ id, content }) => (
+      {props.lyrics.map(({ id, content, likes }) => (
         <li key={id} className="collection-item">
           {content}
+          <div>
+            <i className="material-icons" onClick={() => likeHandler(id)}>
+              thumb_up
+            </i>
+            {likes}
+          </div>
         </li>
       ))}
     </ul>
   );
 };
 
-export default LyricList;
+export default graphql(likeLyric)(LyricList);
